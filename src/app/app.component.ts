2d7a8d985e179b8412c9d780eab009d3ccde9c6a
sazzadh88 <sazzadh88@gmail.com>
2019-09-17 12:44:57 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HttpClient]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  response:any;
+
   title = 'RailwayAPI';
+
+  URL = 'YOUR_API_URL';
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient.get(this.URL).subscribe((res: any[]) => {
+      this.response = JSON.stringify(res);
+    });
+
+  }
 }
